a6b8a81a604f3f5806fee93d99d6c6c414ab3588
michael <jaeyoun.chang@icloud.com>
2023-06-03 21:48:26 +0900

    vlookup update

--- vlookup.py
+++ vlookup.py
@@ -1,46 +1,192 @@
-def vlookup (df_main, df_val, col_key, col_val = None, nan_val = False):
+def vlookup (
+    df_main, df_lookup, lookup_col = None,  
+    left_key = None, right_key = None, org_key = False, 
+    fill_na = False, intsec = 'replace'
+    ):
     '''
-    function like vlookup of Excel
-    df_main: df to receive vlookup Score
-    df_val: df to give vlookup Score
-    col_key: str or list, column(s) of vlookup key (both of 2 dataframes must have this column(s))
-    col_val: str or list, column(s) of vlookup Score
-    nan_val: Score when vlookup result is np.nan, default as np.nan
-    '''
+    function for processing like Excel's vlookup
 
-    col_key = col_key if type(col_key) == list else [col_key]
+    df_main: df to receive vlookup values
+    df_lookup: df to give vlookup values
+
+    left_key: str / list, mapping column(s) in df_main, default as 1st column of df_lookup
+    right_key: str / list, mapping column(s) in df_lookup, default as left_key
+    org_key : boolean, selection of using original key(s) or not, default as False
+    * default value (org_key = False) 
+    enables using modified left/right_keys by fillna('').astype(str).str.strip().str.upper()
+
+    lookup_col = str / list, column(s) of lookup values in df_lookup, default as all columns except right_key
+
+    col_key: str / list, column(s) of vlookup key (both of 2 dataframes must have this column(s))
+    col_val: str / list, column(s) of vlookup value
+    fill_na: value when vlookup result is null, default as null
+
+    # update: str, update added to lookup_col to keep intersection columns in df_main and df_lookup, default as blank
+    # * default value (update = '')
+    # *
+    # *
+    '''
+    ### create df_output
+    df_output = df_main.copy()
+
+
+
+    ### left_key
+    # default as 1st column of df_lookup
+    if left_key is None:
+        left_key = df_lookup.columns[0]
     
-    if col_val is None:
-        col_val = list(filter(lambda x: x not in col_key, df_val.columns))
-        # col_val = list(set(df_val.columns) - set(col_key))
-    else:    
-        col_val = col_val if type(col_val) == list else [col_val]
+    # enlist left_key
+    left_key = left_key if type(left_key) == list else list(left_key)
+
+    
+    
+    ### right_key
+    # default as left_key
+    if right_key is None:
+        right_key = left_key
+    
+    # enlist right_key
+    right_key = right_key if type(right_key) == list else list(right_key)
+    
+    
+    
+    ### lookup_col and modify df_lookup
+    # default as none-key columns of df_lookup
+    if lookup_col is None:
+        lookup_col = list(x for x in df_lookup.columns if x not in right_key)
+
+    # enlist lookup_col
+    lookup_col = lookup_col if type(lookup_col) == list else list(lookup_col)
+    
+    # modify df_lookup to contain right_key and lookup_col
+    df_lookup = df_lookup[right_key + lookup_col]    
+    
+    
+    
+    ### columns of df_output:
+    # 1) left_key
+    # 1-1) left_key (original)
+    # 1-2) left_key modified
+    # 2) none-key columns: output_col
+    # 2-1) output_col exclusively in df_output: output_col_excl
+    # 2-2) output_col intersection with df_lookup: intsec_col
+    #
+    # categorize columns
+    output_col = list(x for x in df_output.columns if x not in left_key)
+    # output_col_excl = list(x for x in output_col if x not in lookup_col)
+    intsec_col = list(x for x in output_col if x in lookup_col)
+    # df_output_col_excl = list(x for x in df_output.columns if x not in intsec_col)
+    
+    
+    
+    ### columns of df_lookup:
+    # 3) right_key
+    # 3-1) right_key (original)
+    # 3-2) right_key modified
+    # 4) none-key columns: lookup_col
+    # 4-1) lookup_col exclusively in df_lookup: lookup_col_excl
+    # 4-2 == 2-2) lookup_col intersection with df_output: intsec_col
+    # 5) other columns not belonging to both of right_key and lookup_col in case lookup_col is designated
+    # 
+    # categorize columns
+    lookup_col_excl = list(x for x in lookup_col if x not in intsec_col)
+
+    
+            
+    ### org_key
+    # with default False create modified key columns
+    if org_key is False:
+       
+        left_key_modified = list()
+        for i in left_key:
+            df_output[i + '_'] = df_output[i].fillna('').astype(str).str.strip().str.upper()
+            left_key_modified.append(i + '_')
+        left_key = left_key_modified
+        # df_output columns:    1-1) left_key (original), 1-2) left_key_modified,
+        #                       2-1) output_col_excl, 2-2) intsec_col
         
-    elements_to_drop = list(set(df_main.columns).intersection(col_val))
-    col_val = [x for x in col_val if x not in elements_to_drop]
+        right_key_modified = list()
+        for i in right_key:
+            df_lookup[i + '_'] = df_lookup[i].fillna('').astype(str).str.strip().str.upper()
+            right_key_modified.append(i + '_')
+        # drop unnecessary right_key (original)
+        df_lookup = df_lookup.drop(right_key, axis = 1)
+        right_key = right_key_modified
+        # df_lookup columns:    3-2) right_key_modified, 
+        #                       4-1) lookup_col_excl, 2-2) intsec_col
+
+    # in case using original columns as keys, create empty lists for dropping after merge
+    else:
+        left_key_modified = list()
+        right_key_modified = list()
+        # df_output columns:    1-1) left_key (original), 2-1) output_col_excl, 2-2) intsec_col
+        # df_lookup columns:    3-1) right_key (original), 3-1) lookup_col_excl, 2-2) intsec_col
+
+
+        
+    ### merge conditions based on intsec
+    if intsec == 'replace':
+        df_output = df_output.drop(intsec_col, axis = 1)    
+        suffix = ''
+        # after merge:  
+        # - on df_output with 1-1) left_key (original), 2-1) output_col_excl and without 2-2) intsec_col 
+        #   new values of 4) lookup_col from df_lookup to be added
+        # - columns except intsec_col to keep original order
+        
+    elif intsec == 'copy':
+        suffix = '_'
+        # after merge:  
+        # - on df_output of its original shape with 1-1) left_key (original), 2-1) output_col_excl, 2-2) intsec_col 
+        #   new values of 4-1) lookup_col_excl, 2-2) intsec_col with '_' suffix from df_lookup to be added
+        # - all columns to keep original order
     
-    df_map = df_val[col_key + col_val].copy()
-    df_result = df_main.copy()
+    elif intsec == 'update':
+        suffix = '_'
+        # after merge:  
+        # - on df_output with 1-1) left_key (original), 2-1) output_col_excl and without 2-2) intsec_col 
+        #   new values of 4-1) lookup_col_excl from df_lookup to be added
+        #   and values of 2-2) intsec_col to be updated
+        # - updated values of 2-2) intsec_col to contain new values of lookup_col if they are provided
+        #   and original values of df_output if lookup_col values are not provided
+        # - columns except intsec_col to keep original order 
     
-    df_result = df_result.merge(
-        df_map,
-        on = col_key,
+    
+    
+    ### merge
+    df_output = df_output.merge(
+        df_lookup,
+        left_on = left_key,
+        right_on = right_key,
         how = 'left',
-        # suffixes=('', '_')
-        )
+        suffixes = ('', suffix)
+        ).drop(left_key_modified,axis = 1)
     
-    if nan_val:
-        df_result[col_val] = df_result[col_val].fillna(nan_val)
     
-    return df_result
-
-    # map_dict = df_val[[col_key, col_val]].set_index(col_key)[col_val].to_dict()
-
-    # df_mapped = df_main.copy()
-    # df_mapped[col_val] = df_mapped[col_key].map(map_dict)
     
-    # if nan_val:
-    #     df_mapped[col_val] = df_mapped[col_val].fillna(nan_val)
+    ### fill_na and reshape based on intsec
+    # enlist fill_na
+    if fill_na:
+        fill_na = fill_na if type(fill_na) == list else [fill_na] * len(lookup_col)   
     
-    # return df_mapped
-
+    # apply fill_na and reshape based on intsec 
+    if intsec == 'replace':
+        for i, j in zip(lookup_col, fill_na):
+            df_output[i] = df_output[i].fillna(j)
+    
+    else:
+        lookup_col_modified = list(x if x in lookup_col_excl else x + '_' for x in lookup_col)
+        
+        if intsec == 'copy':
+            for i, j in zip(lookup_col_modified, fill_na):
+                df_output[i] = df_output[i].fillna(j)
+    
+        elif intsec == 'update':
+            for i, j in zip(lookup_col_modified, lookup_col):
+                df_output[i] = df_output[i].fillna(df_output[j])
+            
+            df_output_col = list(x for x in df_output.columns if x not in intsec)
+            df_output = df_output[df_output_col]
+            
+            df_output_col = list(x.replace('_', '') for x in df_output.columns)
+            df_output.columns = df_output_col
---
+++ vlookup_.py
@@ -0,0 +1,46 @@
+def vlookup (df_main, df_val, col_key, col_val = None, nan_val = False):
+    '''
+    function like vlookup of Excel
+    df_main: df to receive vlookup Score
+    df_val: df to give vlookup Score
+    col_key: str or list, column(s) of vlookup key (both of 2 dataframes must have this column(s))
+    col_val: str or list, column(s) of vlookup Score
+    nan_val: Score when vlookup result is np.nan, default as np.nan
+    '''
+
+    col_key = col_key if type(col_key) == list else [col_key]
+    
+    if col_val is None:
+        col_val = list(filter(lambda x: x not in col_key, df_val.columns))
+        # col_val = list(set(df_val.columns) - set(col_key))
+    else:    
+        col_val = col_val if type(col_val) == list else [col_val]
+        
+    elements_to_drop = list(set(df_main.columns).intersection(col_val))
+    col_val = [x for x in col_val if x not in elements_to_drop]
+    
+    df_map = df_val[col_key + col_val].copy()
+    df_result = df_main.copy()
+    
+    df_result = df_result.merge(
+        df_map,
+        on = col_key,
+        how = 'left',
+        # suffixes=('', '_')
+        )
+    
+    if nan_val:
+        df_result[col_val] = df_result[col_val].fillna(nan_val)
+    
+    return df_result
+
+    # map_dict = df_val[[col_key, col_val]].set_index(col_key)[col_val].to_dict()
+
+    # df_mapped = df_main.copy()
+    # df_mapped[col_val] = df_mapped[col_key].map(map_dict)
+    
+    # if nan_val:
+    #     df_mapped[col_val] = df_mapped[col_val].fillna(nan_val)
+    
+    # return df_mapped
+
