e8ea39085a4fc7b84da282c067d5019d52521b49
michael <jaeyoun.chang@icloud.com>
2023-06-05 01:14:39 +0900

    4jun

--- view_all.py
+++ view_all.py
@@ -1,11 +1,26 @@
 import pandas as pd
+import numpy as np
 from IPython.display import display
+import sys
+from contextlib import ExitStack
 
-def view_all (df):
+def view_all (data):
     '''
-    function to display all contents of a dataframe
+    function to display all contents of data
     '''
-        
+    # with ExitStack() as stack:
+    #     stack.enter_context(pd.option_context(
+    #     'display.max_rows', None,
+    #     'display.max_colwidth', None,
+    #     'display.max_seq_items', None
+    #     ))
+    #     stack.enter_context(np.printoptions(threshold=np.inf))
+    
+    # np.set_printoptions(threshold = sys.maxsize)
+    
     with pd.option_context(
-    'display.max_rows', None, 'display.max_colwidth', None):
-        display(df)
+    'display.max_rows', None, 
+    'display.max_colwidth', None,
+    'display.max_seq_items', None,
+    ):
+        display(data)
